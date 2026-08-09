8acc01b429ce460809cc6604ce8eb0780bc692e8
GameLaxx <tmboisse@gmail.com>
2026-08-09 17:27:34 +0200

    Side bar filled up with dummy

--- front/static/js/index.js
+++ front/static/js/index.js
@@ -1,9 +1,20 @@
 const pills = document.querySelector(".pill-containers").querySelectorAll("div");
+const chats = document.querySelectorAll(".chat-containers > div");
 
 pills.forEach(pill => {
     pill.addEventListener("click", e => {
         pills.forEach(pill => {
             pill.classList.remove("active");
+        });
+        e.target.classList.add("active");
+    })
+})
+
+chats.forEach(chat => {
+    console.log(chat)
+    chat.querySelector("div").addEventListener("click", e => {
+        chats.forEach(chat => {
+            chat.querySelector("div").classList.remove("active");
         });
         e.target.classList.add("active");
     })
