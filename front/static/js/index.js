a3988e6842aa66f94a02b1ff47f61752d6a6ec23
GameLaxx <tmboisse@gmail.com>
2026-08-10 00:23:45 +0200

    Working typing zone

--- front/static/js/index.js
+++ front/static/js/index.js
@@ -1,6 +1,9 @@
 const pills = document.querySelector(".pill-containers").querySelectorAll("div");
 const chats = document.querySelectorAll(".chat-containers > div");
 
+const messagePlaceholder = document.getElementById("message-placeholder");
+const messageContent = document.getElementById("message-content");
+
 pills.forEach(pill => {
     pill.addEventListener("click", e => {
         pills.forEach(pill => {
@@ -11,11 +14,18 @@ pills.forEach(pill => {
 })
 
 chats.forEach(chat => {
-    console.log(chat)
     chat.querySelector("div").addEventListener("click", e => {
         chats.forEach(chat => {
             chat.querySelector("div").classList.remove("active");
         });
         e.target.classList.add("active");
     })
+})
+
+messageContent.addEventListener("input", () => {
+    if(messageContent.innerHTML == "<br>"){
+        messagePlaceholder.classList.remove("hidden");
+    }else{
+        messagePlaceholder.classList.add("hidden");
+    }
 })
